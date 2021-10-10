be1506976af151dd4010299353f2473ca66e2d44
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2021-10-11 00:22:13 +0200

    Added support for filtering messages based on timestamp.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 use std::io::prelude::*;
+use std::collections::HashMap;
+use chrono::{NaiveDateTime, DateTime, Utc};
 
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2
+    if args.len() != 4
     {
-        println!("Usage: LiChats <file>");
+        println!("Usage: LiChats <file> <since> <until>");
+        println!("date format is ISO8601 rfc3339, i.e.: 1996-12-19T16:39:57-08:00");
         println!("Press enter to exit ");
         let _ = std::io::stdin().read(&mut [0u8]).unwrap();
         return Ok(());
     }
-    let file = std::fs::File::open(&args[1])?;
-    let file = std::io::BufReader::new(file);
-    let mut rdr = csv::ReaderBuilder::new()
-        .delimiter(b';')
-        .from_reader(file);
-    let mut map = std::collections::HashMap::new();
-    for result in rdr.records() {
+
+    let since = match DateTime::parse_from_rfc3339(&args[2]) {
+        Ok(since) => since,
+        Err(_) => panic!("Since: Expected date format is ISO8601 rfc3339, i.e.: 1996-12-19T16:39:57-08:00")
+    };
+
+    let until = match DateTime::parse_from_rfc3339(&args[3]) {
+        Ok(since) => since,
+        Err(_) => panic!("Until: Expected date format is ISO8601 rfc3339, i.e.: 1996-12-19T16:39:57-08:00")
+    };
+
+
+    let mut reader = csv::ReaderBuilder::new().flexible(true).from_path(&args[1])?;
+
+    let mut map = HashMap::new();
+    for result in reader.records() {
         let record = result?;
-        *map.entry(record[0].to_owned()).or_insert(0) += 1;
+        match NaiveDateTime::parse_from_str(&record[5], "%Y-%m-%d %H:%M:%S UTC") {
+            Ok(t) => {
+                let t_utc = DateTime::<Utc>::from_utc(t, Utc);
+                if since <= t_utc && t_utc <= until { *map.entry(record[0].to_owned()).or_insert(0) += 1 }
+            }
+            Err(e) => panic!("error parsing record: {:?} with {:?}", record, e)
+        }
     }
     let mut conversations_with_single_msg = 0;
     let mut convestations_with_multiple_msg = 0;
